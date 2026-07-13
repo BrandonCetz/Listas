82f960fee466c668355700e9ef4c9bbd510b598d
Brandon Cetz <guadaponcetz@gmail.com>
2026-07-13 13:36:23 -0600

    ColaArray.java Lista 2.0

--- ColaArray.java
+++ ColaArray.java
@@ -6,11 +6,11 @@ public class ColaArray<E> implements Cola<E>{
     public ColaArray(){
         elementos = new ArrayList<E>();
     } 
-
+    @Override
     public void encolar(E elemento){
         elementos.agregarInicio(elemento);
     }
-
+    @Override
     public E desencolar(){
         if(!elementos.esVacia()){
             return elementos.eliminarElementoFinal();
@@ -18,19 +18,19 @@ public class ColaArray<E> implements Cola<E>{
             throw new NoSuchElementException("No hay elementos en la cola");
         }
     }
-
+    @Override
     public E consultarFrente(){
         return elementos.consultar(elementos.numElementos()-1);
     }
-
+    @Override
     public boolean esVacia(){
         return elementos.esVacia();
     }
-
+    @Override
     public int numElementos(){
         return elementos.numElementos();
     }
-
+    @Override
     public void limpiar(){
         elementos.limpiarLista();
     }
